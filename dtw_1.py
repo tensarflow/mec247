b9660a45e5d0b3a771c2f2df6fd69bb8beb8a1e7
Ensar <te.ozkaya@gmail.com>
2018-10-17 17:54:55 +0900

    Plotting edited

--- dtw_1.py
+++ dtw_1.py
@@ -2,6 +2,7 @@
 
 import math
 import matplotlib.pyplot as plt
+import matplotlib.animation as anim
 import socket
 
 
@@ -9,10 +10,10 @@ def initSocket():
     """Initialize the tcp/ip socket"""
     host = "127.0.0.1"
     port = 5000
-     
+
     mySocket = socket.socket()
     mySocket.bind((host,port))
-     
+
     mySocket.listen(1)
     global conn
     conn, addr = mySocket.accept()
@@ -20,22 +21,22 @@ def initSocket():
 
 def localDistance(templateFrame, testFrame):
     """Compute the Euclidian distance between two feature 'vectors'"""
-    
+
     assert type(templateFrame) == type(testFrame) == float
-    
+
     return math.sqrt(pow(templateFrame - testFrame, 2))
 
 def prettyPrintingArray(a):
     """Print an array with nice line wrapping"""
-    
+
     for i in range(len(a)):
         for j in range(len(a[i])):
             print(a[i][j], end='')
         print("|")
-            
+
 
 def dtw(template, test):
-    
+
     # A data structure to hold the shortest "global distance so far"
     # this is an array (implemented as a list of lists)
     globalDistance = []
@@ -45,10 +46,10 @@ def dtw(template, test):
         for j in range(len(test)):
             thisRow.append(initialValue)
         globalDistance.append(thisRow)
-        
+
 #    print ("Initialized global distance")
 #    prettyPrintingArray(globalDistance)
-    
+
     # second matrix (list of lists) to track the shortest path. Its going to hold the coordinates of the previous point
     backpointer = []
     initialBackpointer = (None, None)
@@ -59,109 +60,121 @@ def dtw(template, test):
         backpointer.append(thisRow)
 #    print("initialized backpointer:")
 #    prettyPrintingArray(backpointer)
-    
+
     # visit every position in the global distance matrix in order
-    for i in range(len(template)): 
-        for j in range(len(test)):            
-            
+    for i in range(len(template)):
+        for j in range(len(test)):
+
             # deal with the edge cases first
             # Starting point: total distance is the local distance because there are no other incoming paths
             if (i==0) and (j==0):
                 globalDistance[i][j] = localDistance(template[i], test[j])
                 backpointer[i][j] = (None, None)
-            
+
             # incoming path comes from one direction when i=0
             elif (i==0):
                 # check if previous position has been visited
                 assert globalDistance[i][j-1] >= 0
-                
+
                 globalDistance[i][j] = globalDistance[i][j-1] + localDistance(template[i], test[j])
                 backpointer[i][j] = (i, j-1)
-              
+
             # incoming path comes from one direction when j=0
             elif (j==0):
                 # check if previous position has been visited
                 assert globalDistance[i-1][j] >= 0
-                
+
                 globalDistance[i][j] = globalDistance[i-1][j] + localDistance(template[i], test[j])
                 backpointer[i][j] = (i-1, j)
-            
+
             # the general case where paths come from three directions
             else:
                 # check if previous positions has been visited
                 assert globalDistance[i][j-1]   >= 0
                 assert globalDistance[i-1][j]   >= 0
-                assert globalDistance[i-1][j-1] >= 0 
-                
+                assert globalDistance[i-1][j-1] >= 0
+
                 lowestGlobalDistance = globalDistance[i-1][j]
                 backpointer[i][j] = (i-1, j)
-                
+
                 if globalDistance[i][j-1] < lowestGlobalDistance:
                     lowestGlobalDistance = globalDistance[i][j-1]
                     backpointer[i][j] = (i, j-1)
-                
+
                 if globalDistance[i-1][j-1] < lowestGlobalDistance:
                     lowestGlobalDistance = globalDistance[i-1][j-1]
                     backpointer[i][j] = (i-1, j-1)
-                
+
                 globalDistance[i][j] = lowestGlobalDistance + localDistance(template[i], test[j])
-    
-    # the best global distance is just the value in the "last" corner of the matrix  
+
+    # the best global distance is just the value in the "last" corner of the matrix
     D = globalDistance[len(template)-1][len(test)-1]
-    
+
     # now do the backtrace
     alignment=[]
-    
+
     # start at the end - the last frame of the template aligns with the last frame of the test signal
     i,j = len(template)-1 , len(test)-1
     alignment.append( (i,j) )
-    
+
     while ( (i!=0) or (j!=0) ):
         alignment.append(backpointer[i][j])
         i,j = backpointer[i][j]               # Knackpunkt(!): i,j momentaner punkt; backpointer[i][j] späterer punkt
-    
+
     alignment.reverse()
-    
+
     return D, alignment
-    
+
 def main():
     initSocket()
-    
+
     template = [0.00079342884228238400, -0.64797733408535600000, 7.51569652570030000000, 8.13736043805227000000, -0.65270681519082600000, 6.78654697206245000000, 8.05186411919532000000, -1.50192128219591000000]
-    
+
     print("Getting data.....")
     receiving = True
+
+    fig = plt.gcf()
+    fig.show()
+    fig.canvas.draw()
+
+
     while receiving:
         data=[]
         for i in range(8):
             dataRec = conn.recv(1024).decode()
-            
+
             if str(dataRec) == "END":
                 receiving = False
                 print("All data received!", str(dataRec))
                 break
-            
+
             elif not dataRec:
                 print("Couldn't receive data!")
                 conn.send("0".encode())
-            
+
             else:
                 data.append(float(dataRec))
                 conn.send("1".encode())
-                
+
         if receiving == True:
-            print("Length Data: ", len(data), "Length template: ", len(template))
             D, alignment = dtw(template, data)
-            print ("Distance from template to test is", D)
-    
-            plt.plot(data)    
+
+            if D < 12:
+                plt.plot(data, 'r-', linewidth = 5)
+            else:
+                plt.plot(data, 'm-')
+
             plt.plot(template)
-            plt.ylabel('data / template')
-            plt.show()
-        
+            plt.title("Distance from template to test: " + str(D))
+
+
+            fig.canvas.draw()
+            fig.clear()
+
+
     conn.close()
-    
-    
-    
+
+
+
 if __name__ == "__main__":
     main()
